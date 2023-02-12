1e4c66f9504a7ab721e0adac13d1536814902db9
nak <nak@home.local>
2023-02-12 15:04:39 +0900

    .env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    AWS_EXPORTS_REGION: process.env.AWS_EXPORTS_REGION,
+    AWS_EXPORTS_USER_POOL_ID: process.env.AWS_EXPORTS_USER_POOL_ID,
+    AWS_EXPORTS_USER_POOLS_WEB_CLIENT_ID: process.env.AWS_EXPORTS_USER_POOLS_WEB_CLIENT_ID,
+  }
 }
 
 module.exports = nextConfig
