bcc48ffcd560e583b016451ca646ddcffc6d5055
nak <nak@home.local>
2023-02-12 15:17:30 +0900

    .env

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -2,8 +2,8 @@ const awsmobile = {
   "aws_project_region": "YOUR_REGION",
   "aws_cognito_identity_pool_id": "YOUR_IDENTITY_POOL_ID",
   "aws_cognito_region": "ap-northeast-1",
-  "aws_user_pools_id": process.env.AWS_EXPORTS_USER_POOL_ID,
-  "aws_user_pools_web_client_id": process.env.AWS_EXPORTS_USER_POOLS_WEB_CLIENT_ID,
+  "aws_user_pools_id": process.env.NEXT_AWS_EXPORTS_USER_POOL_ID,
+  "aws_user_pools_web_client_id": process.env.NEXT_AWS_EXPORTS_USER_POOLS_WEB_CLIENT_ID,
   "oauth": {}
 }
 
